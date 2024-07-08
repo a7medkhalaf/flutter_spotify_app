f77c70fb7e8296e92af03b47150e61304e580b8c
a7medkhalaf <ahmedmohamedkhalaf2020@gmail.com>
2024-07-08 13:19:28 +0300

    Add splash page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spotify_app/core/config/theme/app_themes.dart';
+import 'package:flutter_spotify_app/presentation/splash/pages/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Spotify Demo',
       theme: AppTheme.darkTheme,
-      home: const Placeholder(),
+      home: const SplashPage(),
     );
   }
 }
